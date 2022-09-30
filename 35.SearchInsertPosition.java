c2208da5046c080f73cf0c0f113a257e2116159a
Sharanyu <41756221+Sharanyu@users.noreply.github.com>
2022-09-30 00:13:56 -0400

    Create 35.SearchInsertPosition.java

---
+++ 35.SearchInsertPosition.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0;
+        int j=0;
+        int end=nums.length-1;
+
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+                
+            }
+            if(nums[mid]<target) 
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return start;
+}
+}
